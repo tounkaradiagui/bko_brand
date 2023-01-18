8e0ae8cf4cd48339179994b2de84c377571d1286
diagui <tounkarakan@gmail.com>
2023-01-18 23:16:01 +0300

    displaying Cart item price and also decrement an increment the quantity of the Cart

--- app/Http/Livewire/Frontend/Cart/CartShow.php
+++ app/Http/Livewire/Frontend/Cart/CartShow.php
@@ -8,7 +8,129 @@ use Illuminate\Support\Facades\Auth;
 
 class CartShow extends Component
 {
-    public $cart;
+    public $cart, $totalprice = 0;
+
+    public function removeCartItem(int $cartId)
+    {
+        $removeCart = Cart::where('user_id', auth()->user()->id)->where('id', $cartId)->first();
+        if ($removeCart) {
+            $removeCart->delete();
+            $this->emit('cartAddedUpdated');
+            $this->dispatchBrowserEvent('message', [
+                'text' => "Félicitation ! Le produit a été supprimé à votre Panier",
+                'type' => 'success',
+                'status' => 200
+            ]);
+        }else {
+            $this->dispatchBrowserEvent('message', [
+                'text' => "Erreur de suppression, Veuillez réessayer",
+                'type' => 'error',
+                'status' => 500
+            ]);
+        }
+    }
+
+    public function decrementQuantity(int $cartId)
+    {
+        $cartData = Cart::where('id', $cartId)->where('user_id', auth()->user()->id)->first();
+        if($cartData)
+        {
+            if($cartData->productColor()->where('id', $cartData->product_color_id)->exists()) {
+                $productColor = $cartData->productColor()->where('id', $cartData->product_color_id)->first();
+                if($productColor->quantite > $cartData->quantity)
+                {
+                    $cartData->decrement('quantity');
+                    $this->dispatchBrowserEvent('message', [
+                        'text' => 'Quantité modifiée avec succès !',
+                        'type' => 'success',
+                        'status' => 200
+                    ]);
+                }else {
+                    $this->dispatchBrowserEvent('message', [
+                        'text' => "Il n'y a que" .$productColor->quantite. "Quantité disponible!",
+                        'type' => 'success',
+                        'status' => 200
+                    ]);
+                }
+
+            }else {
+                if ($cartData->product->quantity > $cartData->quantity) 
+                {
+                    $cartData->decrement('quantity');
+                    $this->dispatchBrowserEvent('message', [
+                        'text' => 'Quantité modifiée avec succès !',
+                        'type' => 'success',
+                        'status' => 200
+                    ]);
+                }else {
+                    $this->dispatchBrowserEvent('message', [
+                        'text' => "Il n'y a que" .$cartData->product->quantity. "Quantité disponible!",
+                        'type' => 'success',
+                        'status' => 200
+                    ]);
+                }
+            }
+
+        }else
+        {
+            $this->dispatchBrowserEvent('message', [
+                'text' => 'Erreur de modification, Veuillez réessayer!',
+                'type' => 'error',
+                'status' => 404
+            ]);
+        }
+    }
+
+    public function incrementQuantity(int $cartId)
+    {
+        $cartData = Cart::where('id', $cartId)->where('user_id', auth()->user()->id)->first();
+        if($cartData)
+        {
+            if($cartData->productColor()->where('id', $cartData->product_color_id)->exists()) {
+                $productColor = $cartData->productColor()->where('id', $cartData->product_color_id)->first();
+                if($productColor->quantite > $cartData->quantity)
+                {
+                    $cartData->increment('quantity');
+                    $this->dispatchBrowserEvent('message', [
+                        'text' => 'Quantité modifiée avec succès !',
+                        'type' => 'success',
+                        'status' => 200
+                    ]);
+                }else {
+                    $this->dispatchBrowserEvent('message', [
+                        'text' => "Il n'y a que" .$productColor->quantite. "Quantité disponible!",
+                        'type' => 'success',
+                        'status' => 200
+                    ]);
+                }
+
+            }else {
+                if ($cartData->product->quantity > $cartData->quantity) 
+                {
+                    $cartData->increment('quantity');
+                    $this->dispatchBrowserEvent('message', [
+                        'text' => 'Quantité modifiée avec succès !',
+                        'type' => 'success',
+                        'status' => 200
+                    ]);
+                }else {
+                    $this->dispatchBrowserEvent('message', [
+                        'text' => "Il n'y a que" .$cartData->product->quantity. "Quantité disponible!",
+                        'type' => 'success',
+                        'status' => 200
+                    ]);
+                }
+            }
+
+        }else
+        {
+            $this->dispatchBrowserEvent('message', [
+                'text' => 'Erreur de modification, Veuillez réessayer!',
+                'type' => 'error',
+                'status' => 404
+            ]);
+        }
+    }
 
     public function render()
     {
--- resources/views/livewire/frontend/cart/cart-show.blade.php
+++ resources/views/livewire/frontend/cart/cart-show.blade.php
@@ -9,7 +9,7 @@
 
                         <div class="cart-header d-none d-sm-none d-mb-block d-lg-block">
                             <div class="row">
-                                <div class="col-md-6">
+                                <div class="col-md-4">
                                     <h4>Produits</h4>
                                 </div>
                                 <div class="col-md-2">
@@ -18,16 +18,19 @@
                                 <div class="col-md-2">
                                     <h4>Quantités</h4>
                                 </div>
+                                 <div class="col-md-2">
+                                    <h4>Total</h4>
+                                </div>
                                 <div class="col-md-2">
                                     <h4>Supprimer</h4>
                                 </div>
                             </div>
                         </div>
-                        @forelse ($cart as $cartItem )
-                            @if ($cartItem->product)
+                        @forelse($cart as $cartItem)
+                            @if($cartItem->product)
                                 <div class="cart-item">
                                     <div class="row">
-                                        <div class="col-md-6 my-auto">
+                                        <div class="col-md-4 my-auto">
                                             <a href="{{url('collections/'.$cartItem->product->category->slug.'/'.$cartItem->product->slug)}}">
                                                 <label class="product-name">
                                                     @if($cartItem->product->productImages)
@@ -45,31 +48,63 @@
                                             </a>
                                         </div>
                                         <div class="col-md-2 my-auto">
-                                            <label class="price">${{$cartItem->product->prix_de_vente}} </label>
+                                            <label class="price">{{$cartItem->product->prix_de_vente}} F CFA</label>
                                         </div>
+                                        
                                         <div class="col-md-2 col-7 my-auto">
                                             <div class="quantity">
                                                 <div class="input-group">
-                                                    <span class="btn btn1"><i class="fa fa-minus"></i></span>
-                                                    <input type="text" value="{{$cartItem->quantity}}" class="input-quantity" readonly/>
-                                                    <span class="btn btn1"><i class="fa fa-plus"></i></span>
+                                                    <button type="button" wire:loading.attr="disabled" wire:click="decrementQuantity({{$cartItem->id}})" class="btn btn1"><i class="fa fa-minus"></i></button>
+                                                    <input type="text" value="{{$cartItem->quantity}}" class="input-quantity"/>
+                                                    <button type="button" wire:loading.attr="disabled" wire:click="incrementQuantity({{$cartItem->id}})" class="btn btn1"><i class="fa fa-plus"></i></button>
                                                 </div>
                                             </div>
                                         </div>
+                                        <div class="col-md-2 my-auto">
+                                            <label class="price">{{$cartItem->product->prix_de_vente * $cartItem->quantity}} F CFA </label>
+                                             @php
+                                                $totalprice += $cartItem->product->prix_de_vente * $cartItem->quantity
+                                            @endphp
+                                        </div>
                                         <div class="col-md-2 col-5 my-auto">
                                             <div class="remove">
-                                                <a href="" class="btn btn-danger btn-sm">
-                                                    <i class="fa fa-trash"></i> Remove
-                                                </a>
+                                                <button type="button" wire:loading.attr="disabled" wire:click="removeCartItem({{$cartItem->id}})" class="btn btn-danger btn-sm">
+                                                    <span wire:loading.remove wire:target="removeCartItem({{$cartItem->id}})">
+                                                    <i class="fa fa-trash"></i> Supprimer
+                                                    </span>
+
+                                                    <span wire:loading wire:target="removeCartItem({{$cartItem->id}})">
+                                                        <i class="fa fa-trash"></i>
+                                                        En cours de suppression...
+                                                    </span>
+                                                </button>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             @endif
-                         @empty
+                        @empty
                             <div>Aucun produit disponible</div>
                         @endforelse
                                 
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+
+                <div class="col-md-8 my-md-auto mt-3">
+                    <h4>
+                        Get the best deals & Offers <a href="{{url('/collections')}}">Shop now</a>
+                    </h4>
+                </div>
+                <div class="col-md-4 mt-3">
+                    <div class="shadow-sm bg-white p-3">
+                        <h5>Total :
+                            <span class="float-end">{{$totalprice}} F CFA</span>
+                        </h5>
+                        <hr>
+                        <a href="{{url('/checkout')}}" class="btn btn-warning w-100" >Checkout</a>
                     </div>
                 </div>
             </div>
